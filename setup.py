eb5223f9e575b1d92e1a7e2c5bd40fd9c4abdd32
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-09 16:02:28 +0100

    new version no

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/acdh-oeaw/acdh-obj2xml-pyutils',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
